6e44709074f195a2f34b7ea5d594a3bbc8b1ceba
Eric Zhang <i@qinix.com>
2016-07-23 23:39:48 +0800

    add dashboard page

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard', to: 'dashboard#index'
+
   devise_for :users
   root to: 'home#index'
 
