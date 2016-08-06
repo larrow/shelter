04d863b94f36c96d4aefcd085a5a40da2595b8a0
Eric Zhang <i@qinix.com>
2016-08-06 21:06:43 +0800

    Add pagination for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
 
   def index
     authorize! :read, @repository
-    @tags = @repository.tags.reverse
+    @tags = Kaminari.paginate_array(@repository.tags.reverse).page(params[:page]).per(10)
   end
 
   def destroy
