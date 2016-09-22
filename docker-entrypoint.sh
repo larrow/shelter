80a69bc24b9bba6ef511ca37ad90b9bcc6693e42
fsword <li.jianye@gmail.com>
2016-09-22 10:50:41 +0800

    小修改，简化配置文件，调整启动任务顺序

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,11 @@ else
   sleep 3
   bundle install
 
+  bundle exec rake db:create db:migrate db:seed
+
   if [ "$RAILS_ENV" = "production" ]; then
     bundle exec rake assets:precompile
   fi
 
-  bundle exec rake db:create db:migrate db:seed
   exec rails s -p 3000 -b 0.0.0.0
 fi
