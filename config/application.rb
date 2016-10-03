e3281b132477e6ada8a13b7ffe108facf90f97d5
fsword <li.jianye@gmail.com>
2016-10-04 03:45:22 +0800

    项目初始化以后立刻调用模型方法是不合理的，临时添加一个条件进行约束

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Shelter
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.after_initialize do
-      Repository.delay.sync_from_registry
+      Repository.delay.sync_from_registry if Rails.env.production?
     end
 
     config.i18n.available_locales = %w(en zh-CN)
