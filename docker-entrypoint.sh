10a2f61779a0a116ddd753c9bd8fa4bf05383520
fsword <li.jianye@gmail.com>
2016-10-04 03:30:10 +0800

    镜像版本与线上环境对齐，未来不存在开发版本

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,7 @@ then
 else
   sleep 3
   bundle install
-
   bundle exec rake db:create db:migrate db:seed
-
-  if [ "$RAILS_ENV" = "production" ]; then
-    bundle exec rake assets:precompile
-  fi
 
   exec rails s -p 3000 -b 0.0.0.0
 fi
