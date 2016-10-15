e42df0929738e45afc639958b78618d5c003262b
fsword <li.jianye@gmail.com>
2016-10-15 09:51:40 +0800

    duplicated action: bundle install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,9 @@
 if [ "$1" = "backend" ]
 then
   sleep 5
-  bundle install
   exec bundle exec sidekiq
 else
   sleep 3
-  bundle install
   bundle exec rake db:create db:migrate db:seed
 
   exec rails s -p 3000 -b 0.0.0.0
