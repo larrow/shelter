7d381f610173517af1f4539b78977d9dd2e02d42
James Chen <james@ashchan.com>
2016-10-24 14:50:51 +0900

    Add test for none logged in admin

--- integration_test/spec/admin_dashboard_spec.rb
+++ integration_test/spec/admin_dashboard_spec.rb
@@ -3,31 +3,35 @@ require 'spec_helper'
 RSpec.describe "admin dashboard" do
   let(:agent) { Mechanize.new }
 
-  it 'can see dashboard' do
-    agent.get('http://proxy/').form_with(id: 'new_user') do |form|
-      form['user[login]'] = admin_attrs[:login]
-      form['user[password]'] = admin_attrs[:password]
-    end.submit
+  describe "logged in" do
+    before do
+      log_in_admin(agent)
+    end
 
-    profile_link = agent.get('/')
-      .links
-      .select{|link| link.text == 'Profile'}
-      .first
-    expect(profile_link).to_not be_nil
+    it 'can view dashboard' do
+      profile_link = agent.get('/')
+        .links
+        .select{|link| link.text == 'Profile'}
+        .first
+      expect(profile_link).to_not be_nil
+    end
+
+    it "has no image in a new org" do
+      agent.get('http://proxy/n/new').form_with(id: 'new_group') do |form|
+        form['group[name]'] = 'testorg'
+      end.submit
+
+      image_count = agent.get('http://proxy/testorg').search('.repo-box').length
+      expect(image_count).to eq(0)
+    end
   end
 
-  it "has no image in a new org" do
-    agent.get('http://proxy/').form_with(id: 'new_user') do |form|
-      form['user[login]'] = admin_attrs[:login]
-      form['user[password]'] = admin_attrs[:password]
-    end.submit
-
-    agent.get('http://proxy/n/new').form_with(id: 'new_group') do |form|
-      form['group[name]'] = 'testorg'
-    end.submit
-
-    image_count = agent.get('http://proxy/testorg').search('.repo-box').length
-    expect(image_count).to eq(0)
+  describe "not logged in" do
+    it "cannot view dashboard" do
+      home = agent.get("http://proxy/")
+      profile_link = home.links.select{|link| link.text == 'Profile'}.first
+      expect(profile_link).to be_nil
+    end
   end
 end
 
