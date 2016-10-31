1170406c2874b214a2ff1ab1ba48d7edca6d3724
fsword <li.jianye@gmail.com>
2016-10-31 20:13:17 +0800

    refactory: reuse testcase by shared examples

--- integration_test/spec/registry_spec.rb
+++ integration_test/spec/registry_spec.rb
@@ -7,121 +7,72 @@ ORIGINAL_DIGEST2 = File.read('spec/fixtures/digest2')
 ORIGINAL_MANIFEST_LIBRARY_TEST1 = File.read('spec/fixtures/manifest_library_test1')
 ORIGINAL_MANIFEST_TESTORG_TEST1 = File.read('spec/fixtures/manifest_testorg_test1')
 
-RSpec.describe "registry: push/pull an image" do
-  context "with admin account" do
-    let(:registry) { registry_for(admin_attrs) }
+RSpec.describe "registry operations" do
+  shared_examples 'image maintainer' do
+    it 'can push and pull a image' do
+      registry = registry_for(user)
+      digest1 = registry.push_blob(image, ORIGINAL_BLOB1)
+      digest2 = registry.push_blob(image, ORIGINAL_BLOB2)
+      res = registry.push_manifest(image, 'latest', ORIGINAL_MANIFEST_TESTORG_TEST1)
 
-    describe "push to library" do
-      it "can push" do
-        image = "library/testpush"
+      expect(digest1).to eq(ORIGINAL_DIGEST1)
+      expect(digest2).to eq(ORIGINAL_DIGEST2)
+      expect(res).not_to be_nil
 
-        digest1 = registry.push_blob(image, ORIGINAL_BLOB1)
-        digest2 = registry.push_blob(image, ORIGINAL_BLOB2)
-        res = registry.push_manifest(image, 'latest', ORIGINAL_MANIFEST_LIBRARY_TEST1)
+      blob1 = registry.pull_blob(image, ORIGINAL_DIGEST1)
+      blob2 = registry.pull_blob(image, ORIGINAL_DIGEST2)
+      mani = registry.pull_manifest(image, 'latest')
 
-        expect(digest1).to eq(ORIGINAL_DIGEST1)
-        expect(digest2).to eq(ORIGINAL_DIGEST2)
-        expect(res).not_to be_nil
-      end
-    end
-
-    describe "pull from library" do
-      let(:image) { "library/testpull" }
-      before do
-        registry.push_blob(image, ORIGINAL_BLOB1)
-        registry.push_blob(image, ORIGINAL_BLOB2)
-        registry.push_manifest(image, 'latest', ORIGINAL_MANIFEST_LIBRARY_TEST1)
-      end
-
-      it "can pull" do
-        blob1 = registry.pull_blob(image, ORIGINAL_DIGEST1)
-        blob2 = registry.pull_blob(image, ORIGINAL_DIGEST2)
-        manifest = registry.pull_manifest(image, 'latest')
-
-        expect(blob1.bytes).to eq(ORIGINAL_BLOB1.bytes)
-        expect(blob2.bytes).to eq(ORIGINAL_BLOB2.bytes)
-        expect(manifest).to eq(ORIGINAL_MANIFEST_LIBRARY_TEST1)
-      end
+      expect(blob1.bytes).to eq(ORIGINAL_BLOB1.bytes)
+      expect(blob2.bytes).to eq(ORIGINAL_BLOB2.bytes)
+      expect(mani).to eq(ORIGINAL_MANIFEST_TESTORG_TEST1)
     end
   end
 
-  context 'with user account' do
-    describe 'not signed up' do
-      let(:user_attrs) do
-        {
-          login: next_username,
-          email: next_email,
-          password: "testpassword"
-        }
-      end
+  describe "admin account" do
+    it_should_behave_like 'image maintainer' do
+      let(:user) { admin_attrs }
+      let(:image) { "library/testimage" }
+    end
+  end
 
-      let(:registry) { registry_for(user_attrs) }
+  describe 'anonymous' do
 
-      it 'cannot push to group' do
-        expect {
-          registry.push_blob('testorg/test1', ORIGINAL_BLOB1)
-        }.to raise_error(RestClient::Unauthorized)
-      end
+    let(:registry) { registry_for next_user }
+
+    it 'cannot push to group' do
+      expect {
+        registry.push_blob('testorg/test1', ORIGINAL_BLOB1)
+      }.to raise_error(RestClient::Unauthorized)
+    end
+  end
+
+  describe 'normal user' do
+    let(:user) { next_user }
+
+    before do
+      admin_agent = Mechanize.new
+      sign_up(user, admin_agent)
     end
 
-    describe 'signed up' do
-      let(:username) { next_username }
-      let(:group) { next_group }
+    it_should_behave_like 'image maintainer' do
+      let(:image) { "#{user[:login]}/test1" }
+    end
+  end
 
-      let(:user_attrs) do
-        {
-          login: username,
-          email: next_email,
-          password: "testpassword"
-        }
-      end
+  describe 'group' do
+    let(:user) { next_user }
+    let(:group) { next_group }
 
-      let(:image) { "#{username}/test1" }
-      let(:image_on_group) { "#{group}/test1" }
-      let(:registry) { registry_for(user_attrs) }
+    before do
+      admin_agent = Mechanize.new
+      sign_up(user, admin_agent)
+      create_group(group, admin_agent)
+      add_user_to_group(user, group, admin_agent)
+    end
 
-      before do
-        admin_agent = Mechanize.new
-        sign_up(user_attrs, admin_agent)
-        create_group(group, admin_agent)
-        add_user_to_group(user_attrs, group, admin_agent)
-      end
-
-      it 'can push to and pull a image' do
-        digest1 = registry.push_blob(image, ORIGINAL_BLOB1)
-        digest2 = registry.push_blob(image, ORIGINAL_BLOB2)
-        res = registry.push_manifest(image, 'latest', ORIGINAL_MANIFEST_TESTORG_TEST1)
-
-        expect(digest1).to eq(ORIGINAL_DIGEST1)
-        expect(digest2).to eq(ORIGINAL_DIGEST2)
-        expect(res).not_to be_nil
-
-        blob1 = registry.pull_blob(image, ORIGINAL_DIGEST1)
-        blob2 = registry.pull_blob(image, ORIGINAL_DIGEST2)
-        mani = registry.pull_manifest(image, 'latest')
-
-        expect(blob1.bytes).to eq(ORIGINAL_BLOB1.bytes)
-        expect(blob2.bytes).to eq(ORIGINAL_BLOB2.bytes)
-        expect(mani).to eq(ORIGINAL_MANIFEST_TESTORG_TEST1)
-      end
-
-      it 'can push to and pull on group' do
-        digest1 = registry.push_blob(image_on_group, ORIGINAL_BLOB1)
-        digest2 = registry.push_blob(image_on_group, ORIGINAL_BLOB2)
-        res = registry.push_manifest(image_on_group, 'latest', ORIGINAL_MANIFEST_TESTORG_TEST1)
-
-        expect(digest1).to eq(ORIGINAL_DIGEST1)
-        expect(digest2).to eq(ORIGINAL_DIGEST2)
-        expect(res).not_to be_nil
-
-        blob1 = registry.pull_blob(image_on_group, ORIGINAL_DIGEST1)
-        blob2 = registry.pull_blob(image_on_group, ORIGINAL_DIGEST2)
-        mani = registry.pull_manifest(image_on_group, 'latest')
-
-        expect(blob1.bytes).to eq(ORIGINAL_BLOB1.bytes)
-        expect(blob2.bytes).to eq(ORIGINAL_BLOB2.bytes)
-        expect(mani).to eq(ORIGINAL_MANIFEST_TESTORG_TEST1)
-      end
+    it_should_behave_like 'image maintainer' do
+      let(:image) { "#{group}/test1" }
     end
   end
 end
