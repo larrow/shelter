4bc7601298bad5d82ceae9ba5df20d3ef38af8e9
fsword <li.jianye@gmail.com>
2016-11-07 00:24:34 +0800

    support image tag on test cases.

--- integration_test/features/step_definitions/image_steps.rb
+++ integration_test/features/step_definitions/image_steps.rb
@@ -1,10 +1,11 @@
-那么(/^可以在(.*)中添加镜像$/) do |label|
+那么(/^可以在(.*)中添加镜像，版本为(.*)$/) do |label,tag|
 
-  image = "#{namespaces[label]}/test1"
-  expect{Registry.push image, :v1}.not_to raise_error
+  image = "#{namespaces[label]}/test"
+  expect{Registry.push image, tag}.not_to raise_error
 end
 
-那么(/^可以删除(.*)中的镜像$/) do |label|
-  _image = "#{namespaces[label]}/test1"
+那么(/^可以删除(.*)中前述镜像的(.*)版本$/) do |label,tag|
+  image = "#{namespaces[label]}/test"
+  visit "#{image}/tags"
   pending
 end
