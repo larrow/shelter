43c224d5b05fb667c1e6c79517b660401521b0eb
fsword <li.jianye@gmail.com>
2016-11-17 19:46:46 +0800

    调用脚本，生成必要的秘钥对和secret key信息

--- integration_test/prepare.sh
+++ integration_test/prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+sh -c "cd .. && ./prepare.sh localhost"
+
 rm -rf config
-mkdir -p config
+mkdir config
 cp -a ../config/registry config/
 sed "s/realm: http:\/\/[^\/]*/realm: http:\/\/proxy\.local/" config/registry/config.yml.template > config/registry/config.yml
+
