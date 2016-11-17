41577af36075d849edaf48457ef926f8cc7b79aa
fsword <li.jianye@gmail.com>
2016-11-17 19:59:28 +0800

    重新规划配置文件，确保测试环境和研发保持一致

--- integration_test/prepare.sh
+++ integration_test/prepare.sh
@@ -2,8 +2,6 @@
 
 sh -c "cd .. && ./prepare.sh localhost"
 
-rm -rf config
-mkdir config
-cp -a ../config/registry config/
-sed "s/realm: http:\/\/[^\/]*/realm: http:\/\/proxy\.local/" config/registry/config.yml.template > config/registry/config.yml
+mkdir -p config/registry
+sed "s/realm: http:\/\/[^\/]*/realm: http:\/\/proxy\.local/" ../config/registry/config.yml.template > config/registry/config.yml
 
