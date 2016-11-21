b65a0d0c5afbd8caf398d2c67ab4170501a6e0d7
fsword <li.jianye@gmail.com>
2016-11-21 19:23:52 +0800

    update db files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-admin = User.create(email: 'admin@example.com', username: 'admin', password: 'shelter12345', password_confirmation: 'shelter12345', admin: true)
+admin = User.find_or_create_by(email: 'admin@example.com', username: 'admin', password: 'shelter12345', password_confirmation: 'shelter12345', admin: true)
 admin.create_namespace 'library'
 
