a286e1b9328d8ab1e8aff0c546a67577e86e01d8
fsword <li.jianye@gmail.com>
2016-11-21 19:43:54 +0800

    bugfixed: 确保数据初始化成功，且能重复执行（不会重置旧数据）

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
-admin = User.find_or_create_by(email: 'admin@example.com', username: 'admin', password: 'shelter12345', password_confirmation: 'shelter12345', admin: true)
-admin.create_namespace 'library'
+user_param = {email: 'admin@example.com', username: 'admin', password: 'shelter12345', password_confirmation: 'shelter12345', admin: true}
+admin = User.find_by(user_param.slice :email, :username)
+
+if admin.nil?
+  admin = User.create user_param
+  admin.create_namespace 'library'
+end
 
