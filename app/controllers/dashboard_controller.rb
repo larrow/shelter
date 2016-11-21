d475443b0a46c5c15fade75c252f1ab1e387d670
fsword <li.jianye@gmail.com>
2016-11-21 19:24:40 +0800

    bugfix: 面板页面显示异常

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
     @authorized_groups = current_user.namespaces
+    @repositories = current_user.personal_namespace.repositories.order(updated_at: :desc).page(params[:page])
   end
 end
