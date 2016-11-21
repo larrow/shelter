17bede0135687d73e4a11700e629aa592e7b187e
fsword <li.jianye@gmail.com>
2016-11-21 19:00:19 +0800

    添加旧数据迁移脚本

--- db/migrate/20161120163324_remove_group_type.rb
+++ db/migrate/20161120163324_remove_group_type.rb
@@ -12,5 +12,22 @@ class RemoveGroupType < ActiveRecord::Migration[5.0]
 
     rename_table :group_members, :members
     rename_column :members, :group_id, :namespace_id
+
+    reversible do |dir|
+      dir.up do
+        User.find_each do |u|
+          ns = Namespace.find_by(name: u.username)
+          ns.owners << u unless ns.owners.include?(u)
+        end
+      end
+
+      dir.down do
+        User.find_each do |u|
+          ns = Namespace.find_by(name: u.username)
+          ns.owners.delete u if ns.owners.include?(u)
+        end
+      end
+
+    end
   end
 end
