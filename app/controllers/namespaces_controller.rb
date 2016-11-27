193d7f6bc0bf43564d165ae9596dea2859f90c8e
fsword <li.jianye@gmail.com>
2016-11-27 16:27:46 +0800

    界面修改
    * 个人空间不支持多人协作功能
    * 删除个人空间时，提供错误提示
    * 调整部分提示信息内容

--- app/controllers/namespaces_controller.rb
+++ app/controllers/namespaces_controller.rb
@@ -21,7 +21,8 @@ class NamespacesController < ApplicationController
   def destroy
     authorize! :update, @namespace
     redirect_back fallback_location: dashboard_index_path, alert: t('.library_cannot_delete') and return if @namespace.name == 'library'
-    @namespace.destroy unless @namespace.personal?
+    redirect_back fallback_location: dashboard_index_path, alert: t('.personal_cannot_delete') and return if @namespace.personal?
+    @namespace.destroy
     redirect_to dashboard_index_path, notice: t('.namespace_deleted')
   end
 
