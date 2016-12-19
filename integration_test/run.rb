0f322bcd0dc4aada1065c32e6438ee066656acc4
fsword <li.jianye@gmail.com>
2016-12-19 08:52:32 +0800

    不再添加执行提示

--- integration_test/run.rb
+++ integration_test/run.rb
@@ -2,18 +2,14 @@
 
 require 'net/ping'
 
-$stdout.sync = true
-
 def check_server
   30.times do
     return if Net::Ping::HTTP.new('proxy').ping?
-    puts "."
     sleep 1.5
   end
   fail 'cannot connect to server'
 end
 
-puts 'wait for the service to be ready'
 check_server
 exec 'cucumber'
 
