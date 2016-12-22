6c9aa1c4ae41de5d962def61991a06db76087084
Lester Zhao <zm.backer@gmail.com>
2016-12-22 13:08:31 +0800

    管理员搜索测试补充

--- integration_test/features/step_definitions/user_steps.rb
+++ integration_test/features/step_definitions/user_steps.rb
@@ -67,6 +67,18 @@ end
   end
 end
 
-当(/^管理员搜索(.*)$/) do |k|
-  pending # Write code here that turns the phrase above into concrete actions
+当(/^管理员搜索(.*)$/) do |s|
+  user = users["管理员"]
+  visit "/search?utf8=✓&q=#{s}"
 end
+
+那么(/^搜索结果包括：镜像(\d+)个，用户(\d+)个，用户组(\d+)个$/) do |img, u, g|
+  image_links = page.links.select{|link| link.href =~ /\/n\/[a-zA-Z.0-9_\-]+\/r\/[a-zA-Z.0-9_\-]+/ }
+  expect(image_links.size).to be >= img.to_i
+
+  
+
+
+
+end
+
