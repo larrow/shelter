a0426b1d29e82d560f14935c68e14dd952cbb300
Lester Zhao <zm.backer@gmail.com>
2016-12-22 17:36:58 +0800

    测试错误

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,14 @@
 class SearchController < ApplicationController
 
   def index
-    @repositories = Repository.where(is_public: true).where('name like ?', "%#{params[:q]}%")
-    
+    @repositories = Repository.where(is_public: true)
     if user_signed_in?
       @repositories = @repositories.or(Repository.where(id: current_user.repositories.pluck(:id)))
     end
+    @repositories = @repositories.where('name like ?', "%#{params[:q]}%")
     if current_user&.admin?
       @namespaces = Namespace.where('name like ?', "%#{params[:q]}%")
     end
-
-
   end
 
 
