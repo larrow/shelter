2fa1a2f54fed570ca94e3d8a33f4d7286fe21c78
fsword <li.jianye@gmail.com>
2017-04-18 23:40:50 +0800

    split http/https conf files

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -2,7 +2,7 @@ class ServiceController < ApplicationController
   protect_from_forgery with: :null_session
 
   # ignore csrf params for registry callback
-  skip_before_filter :verify_authenticity_token, :only => [:notifications, :sync]
+  skip_before_action :verify_authenticity_token, :only => [:notifications, :sync]
 
   def notifications
     inner_service_auth do
--- prepare.sh
+++ prepare.sh
@@ -12,7 +12,7 @@ fi
 
 mkdir -p config
 
-app_config="/usr/src/app/config"
+app_config=${APP_CONFIG:-'/usr/src/app/config'}
 
 # copy config files
 if [ -d 'config/nginx' ]
@@ -41,13 +41,35 @@ rm $private_key_pem $root_crt >/dev/null 2>&1
 openssl genrsa -out ${private_key_pem} 4096
 openssl req -new -x509 -key ${private_key_pem} -out ${root_crt} -days 3650 -subj "/CN=${host}"
 
+# generate secret key and service token
 secret_key=$(openssl rand -base64 42)
 echo "SECRET_KEY_BASE=${secret_key}" > config/env_file
 
 service_token=$(openssl rand -hex 42)
 echo "SERVICE_TOKEN=${service_token}" >> config/env_file
 
+# generate https files for nginx
+cd config/nginx/cert
+
+openssl req -subj "/C=CN/ST=Hangzhou/O=company/CN=$host" -newkey rsa:4097 -nodes -sha256 -keyout ca.key -x509 -days 365 -out ca.crt
+openssl req -subj "/C=CN/ST=Hangzhou/O=company/CN=$host" -newkey rsa:4096 -nodes -sha256 -keyout server.key -out server.csr
+
+mkdir demoCA
+cd demoCA
+touch index.txt
+echo '01' > serial
+cd ..
+
+openssl ca -batch -in server.csr -out server.crt -cert ca.crt -keyfile ca.key -outdir .
+rm -rf demoCA server.csr
+
+cd ../../..
+# make nginx.conf
+sed -i '' "s/server_name .*;/server_name $host;/g" config/nginx/http.conf
+sed -i '' "s/server_name .*;/server_name $host;/g" config/nginx/https.conf
+
 # make registry config.yml
 sed "s/realm: http:\/\/[^\/]*/realm: http:\/\/$host/" config/registry/config.yml.template \
   | sed "s/Bearer/Bearer ${service_token}/" > config/registry/config.yml
+
 
