06f0dc2eb423edef4d182116b6fb609188c82193
fsword <li.jianye@gmail.com>
2017-04-25 20:29:52 +0800

    重构，生成caddy相关的配置文件

--- prepare.sh
+++ prepare.sh
@@ -10,23 +10,39 @@ if [[ -z "${host// }" ]]; then
     host="localhost"
 fi
 
-mkdir -p config
+dst_config=`pwd`/config
+mkdir -p $dst_config
 
-app_config=${APP_CONFIG:-'/usr/src/app/config'}
-
-if [ -d 'config/registry' ]
+# check if it run in container
+if [ -d '/usr/src/app/config' ]
 then
-  echo 'has registry conf, ignore'
+  src_config='/usr/src/app/config'
 else
-  cp -r $app_config/registry config/
+  # src and dst config is same folder when developing
+  src_config=$dst_config
+fi
+
+if [ -d "$dst_config/registry" ]
+then
+  echo 'has registry config, ignore'
+else
+  cp -r $src_config/registry $dst_config/
+fi
+
+if [ -d "$dst_config/caddy" ]
+then
+  echo 'has caddy config, ignore'
+else
+  cp -r $src_config/caddy $dst_config/
+  cp -a $src_config/Caddyfile $dst_config/
 fi
 
 # check for openssl
 command -v openssl >/dev/null 2>&1 || { echo >&2 "Require openssl but it's not installed.  Aborting."; exit 1; }
 
 # build key-pair for web and registry
-private_key_pem="config/private_key.pem"
-root_crt="config/registry/root.crt"
+private_key_pem="$dst_config/private_key.pem"
+root_crt="$dst_config/registry/root.crt"
 
 rm $private_key_pem $root_crt >/dev/null 2>&1
 
@@ -35,17 +51,17 @@ openssl req -new -x509 -key ${private_key_pem} -out ${root_crt} -days 3650 -subj
 
 # generate secret key and service token
 secret_key=$(openssl rand -base64 42)
-echo "SECRET_KEY_BASE=${secret_key}" > config/env_file
+echo "SECRET_KEY_BASE=${secret_key}" > $dst_config/env_file
 
 service_token=$(openssl rand -hex 42)
-echo "SERVICE_TOKEN=${service_token}" >> config/env_file
+echo "SERVICE_TOKEN=${service_token}" >> $dst_config/env_file
 
 # set caddy config file
-echo "http://$host {" > config/Caddyfile
-sed '1d' config/Caddyfile.template >> config/Caddyfile
+echo "http://$host {" > $dst_config/Caddyfile
+sed '1d' $src_config/Caddyfile.template >> $dst_config/Caddyfile
 
 # make registry config.yml
-sed "s/realm: http:\/\/[^\/]*/realm: http:\/\/$host/" config/registry/config.yml.template \
-  | sed "s/Bearer/Bearer ${service_token}/" > config/registry/config.yml
+sed "s/realm: http:\/\/[^\/]*/realm: http:\/\/$host/" $src_config/registry/config.yml.template \
+  | sed "s/Bearer/Bearer ${service_token}/" > $dst_config/registry/config.yml
 
 
