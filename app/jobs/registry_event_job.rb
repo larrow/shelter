8f5df4f242f98ee9eba0a67516de375047befb8e
fsword <li.jianye@gmail.com>
2017-09-06 09:42:59 +0800

    补充显示tag的size和更新时间信息

--- app/jobs/registry_event_job.rb
+++ app/jobs/registry_event_job.rb
@@ -8,7 +8,7 @@ class RegistryEventJob < ApplicationJob
       when 'push'
         repo = Repository.find_or_create_by_full_name repository
         tag = repo.tags.find_or_create_by name: event['target']['tag']
-        tag.update digest: event['target']['digest']
+        tag.update digest: event['target']['digest'], size: event['target']['size']
         RepositoryChannel.broadcast_to(repo, action: 'push') if repo
       when 'pull'
         Repository.find_or_create_by_full_name(repository).increment! :pull_count unless event['actor']['name'] == 'system-service'
